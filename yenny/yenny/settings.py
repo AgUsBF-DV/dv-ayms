7267604b38ae841bb7840918a6f0b5ce6796fa78
AgUsBF <agustinbeceyro@gmail.com>
2025-09-21 21:22:30 -0300

    feat: config para linkear djanco con postgres

--- yenny/yenny/settings.py
+++ yenny/yenny/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'yenny.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yenny_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
