3a0b9037435d0a114ca562ccdaee179c893a7127
AgUsBF <agustinbeceyro@gmail.com>
2025-09-29 00:15:39 -0300

    feat: carga de modelo, migracion y vista basica de registros de autor

--- src/model/Autor.py
+++
@@ -1,8 +0,0 @@
-class Autor:
-    def __init__(self, id: int, nombre: str, apellido: str):
-        self.id = id
-        self.nombre = nombre
-        self.apellido = apellido
-
-    def __str__(self):
-        return f"Autor({self.id}, {self.nombre} {self.apellido})"
--- yenny/autores/views.py
+++ yenny/autores/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Autor
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Página de Autores en construcción.")
+    autores = Autor.objects.all()
+    return render(request, 'index.html', {'autores': autores})
