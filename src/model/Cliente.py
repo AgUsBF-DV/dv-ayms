b0b4c962601b81b50b3c382181d91e6f618a881a
AgUsBF <agustinbeceyro@gmail.com>
2025-10-21 23:18:16 -0300

    chores: reorganizar segun estructura de Django

--- src/model/Cliente.py
+++
@@ -1,9 +0,0 @@
-class Cliente:
-    def __init__(self, id: int, nombre: str, apellido: str, correo: str):
-        self.id = id
-        self.nombre = nombre
-        self.apellido = apellido
-        self.correo = correo
-
-    def __str__(self):
-        return f"Cliente({self.id}, {self.nombre} {self.apellido}, {self.correo})"
--- src/test.py
+++
@@ -1,6 +0,0 @@
-
-from model.Rol import Rol
-
-
-rol = Rol(1, "Admin")
-print(rol)
