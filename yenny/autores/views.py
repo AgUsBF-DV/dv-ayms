77cb350c64bd6a0cc877a2842f7b301d1c1c1a74
AgUsBF <agustinbeceyro@gmail.com>
2025-10-27 23:05:33 -0300

    feat: implementar paginación y tabla en la lista de autores

--- yenny/autores/views.py
+++ yenny/autores/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Autor
 
 # Create your views here.
 def index(request):
     autores = Autor.objects.all()
-    return render(request, 'lista-autores.html', {'autores': autores})
+    paginator = Paginator(autores, 10)
+    page_number = request.GET.get('page')
+    objeto_pag = paginator.get_page(page_number)
+
+    columnas = ['ID', 'Nombre', 'Apellido', 'Acciones']
+    registros = []
+    for autor in objeto_pag:
+        registros.append([
+            autor.id,
+            getattr(autor, 'nombre', ''),
+            getattr(autor, 'apellido', ''),
+            '',  # para la botonera
+        ])
+
+    context = {
+        'titulo': 'Lista de Autores',
+        'columnas': columnas,
+        'registros': registros,
+        'objeto_pag': objeto_pag,
+    }
+    return render(request, 'lista-autores.html', context)
