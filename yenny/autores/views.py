f835743d1cc52a34b5ff5b28d3c5c6174ac41e98
AgUsBF <agustinbeceyro@gmail.com>
2025-11-06 19:55:07 -0300

    feat: agregar confirmacion de eliminación de autores, mostrar mensaje de error y detalles en modo solo lectura

--- yenny/autores/views.py
+++ yenny/autores/views.py
@@ -1,11 +1,15 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .forms import AutorForm
 from .models import Autor
+from django.db.models.deletion import ProtectedError
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 class AutorListView(ListView):
     model = Autor
     template_name = 'lista-autores.html'
     paginate_by = 10
+    ordering = ['apellido', 'nombre']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -13,12 +17,13 @@ class AutorListView(ListView):
         objeto_pag = context['page_obj']
         registros = []
         for autor in objeto_pag:
-            registros.append([
+            campos = [
                 autor.id,
                 getattr(autor, 'nombre', ''),
                 getattr(autor, 'apellido', ''),
                 '',  # para la botonera
-            ])
+            ]
+            registros.append({'objeto': autor, 'campos': campos})
         context.update({
             'titulo': 'Lista de Autores',
             'path_crear': '/autores/nuevo/',
@@ -26,7 +31,12 @@ class AutorListView(ListView):
             'columnas': columnas,
             'registros': registros,
             'objeto_pag': objeto_pag,
+            'modelo': 'autor',
         })
+
+        if self.request.GET.get('error') == 'protected':
+            context['error_message'] = "No se puede eliminar el autor porque tiene libros asociados."
+        
         return context
 
 class AutorCreateView(CreateView):
@@ -45,3 +55,25 @@ class AutorDeleteView(DeleteView):
     model = Autor
     template_name = 'autor-confirm-delete.html'
     success_url = '/autores/'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.get_success_url())
+        except ProtectedError:
+            return redirect(self.success_url + '?error=protected')
+
+class AutorShowView(AutorUpdateView):
+    template_name = 'autor-form.html'
+    
+    # Cargar el formulario en modo solo lectura
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        for field in form.fields.values():
+            field.widget.attrs['readonly'] = True
+            field.widget.attrs['disabled'] = True
+        return form
+
+    # No permitir POST (no guardar cambios)
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
