ec70c33ea76b359ee1a62a3127a6786e1c141aa8
AgUsBF <agustinbeceyro@gmail.com>
2025-11-06 21:08:01 -0300

    feat: agregar funcionalidades de los botones ver, editar y eliminar de clientes

--- yenny/clientes/urls.py
+++ yenny/clientes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ClienteListView, ClienteCreateView, ClienteUpdateView, ClienteDeleteView
+from .views import ClienteListView, ClienteCreateView, ClienteUpdateView, ClienteDeleteView, ClienteShowView
 
 urlpatterns = [
     path('', ClienteListView.as_view(), name='cliente-list'),
     path('nuevo/', ClienteCreateView.as_view(), name='cliente-create'),
     path('editar/<int:pk>/', ClienteUpdateView.as_view(), name='cliente-update'),
     path('eliminar/<int:pk>/', ClienteDeleteView.as_view(), name='cliente-delete'),
+    path('ver/<int:pk>/', ClienteShowView.as_view(), name='cliente-show'),
 ]
--- yenny/clientes/views.py
+++ yenny/clientes/views.py
@@ -1,11 +1,15 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .forms import ClienteForm
 from .models import Cliente
+from django.db.models.deletion import ProtectedError
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 class ClienteListView(ListView):
     model = Cliente
     template_name = 'lista-clientes.html'
     paginate_by = 10
+    ordering = ['apellido', 'nombre']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -13,12 +17,13 @@ class ClienteListView(ListView):
         objeto_pag = context['page_obj']
         registros = []
         for cliente in objeto_pag:
-            registros.append([
+            campos = [
                 cliente.id,
                 getattr(cliente, 'nombre', ''),
                 getattr(cliente, 'apellido', ''),
                 '',  # para la botonera
-            ])
+            ]
+            registros.append({'objeto': cliente, 'campos': campos})
         context.update({
             'titulo': 'Lista de Clientes',
             'path_crear': '/clientes/nuevo/',
@@ -26,7 +31,12 @@ class ClienteListView(ListView):
             'columnas': columnas,
             'registros': registros,
             'objeto_pag': objeto_pag,
+            'modelo': 'cliente',
         })
+
+        if self.request.GET.get('error') == 'protected':
+            context['error_message'] = "No se puede eliminar el cliente porque tiene ventas asociadas."
+        
         return context
 
 class ClienteCreateView(CreateView):
@@ -45,3 +55,26 @@ class ClienteDeleteView(DeleteView):
     model = Cliente
     template_name = 'cliente-confirm-delete.html'
     success_url = '/clientes/'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.get_success_url())
+        except ProtectedError:
+            return redirect(self.success_url + '?error=protected')
+
+# Vista de solo lectura para mostrar un cliente
+class ClienteShowView(ClienteUpdateView):
+    template_name = 'cliente-form.html'
+    
+    # Cargar el formulario en modo solo lectura
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        for field in form.fields.values():
+            field.widget.attrs['readonly'] = True
+            field.widget.attrs['disabled'] = True
+        return form
+
+    # No permitir POST (no guardar cambios)
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
