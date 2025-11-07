e31413351d7477f9c48da07efd11dbf93d55b11e
AgUsBF <agustinbeceyro@gmail.com>
2025-11-07 02:01:53 -0300

    feat: implementar restricciones en la botonera y middleware según rol

--- yenny/yenny/middleware.py
+++ yenny/yenny/middleware.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.urls import reverse
+from urllib import request
 
 class LoginRequiredMiddleware:
     """
@@ -7,31 +8,55 @@ class LoginRequiredMiddleware:
     Excluye rutas de login, logout y admin.
     También restringe acceso a rutas de admin para roles específicos.
     """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         # Rutas que no requieren autenticación
         allowed_paths = [
-            reverse('empleado-login'),
-            reverse('empleado-logout'),
+            reverse("empleado-login"),
+            reverse("empleado-logout"),
             # '/admin/',
         ]
 
-        if not request.user.is_authenticated and not any(request.path.startswith(path) for path in allowed_paths):
-            return redirect('empleado-login')
-        
+        if not request.user.is_authenticated and not any(
+            request.path.startswith(path) for path in allowed_paths
+        ):
+            return redirect("empleado-login")
+
         # Rutas que requieren rol ADMIN o ENCARGADO
         admin_encargado_paths = [
-            '/categorias/',
-            '/editoriales/',
-            '/empleados/',
+            "/autores/eliminar/",
+            "/libros/eliminar/",
+            "/clientes/eliminar/",
+            "/ventas/editar/",
+            "/categorias/",
+            "/editoriales/",
+            "/empleados/",
         ]
 
-        if (request.user.is_authenticated and 
-            any(request.path.startswith(path) for path in admin_encargado_paths) and 
-            request.path != '/empleados/logout/'):
-            if request.user.rol not in ['ADMIN', 'ENCARGADO']:
-                return redirect('/')  # Redirigir al inicio
-        
+        if (
+            request.user.is_authenticated
+            and any(request.path.startswith(path) for path in admin_encargado_paths)
+            and request.path != "/empleados/logout/"
+        ):
+            if request.user.rol not in ["ADMIN", "ENCARGADO"]:
+                return redirect("/")  # Redirigir al inicio
+
+        # Rutas que requieren rol ADMIN exclusivamente
+        admin_paths = [
+            "/ventas/eliminar/",
+            "/categorias/eliminar/",
+            "/editoriales/eliminar/",
+            "/empleados/editar/",
+            "/empleados/eliminar/",
+        ]
+
+        if request.user.is_authenticated and any(
+            request.path.startswith(path) for path in admin_paths
+        ):
+            if request.user.rol != "ADMIN":
+                return redirect("/")  # Redirigir al inicio
+
         return self.get_response(request)
