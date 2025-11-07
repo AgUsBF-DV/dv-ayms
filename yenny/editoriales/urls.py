27c5e5fbca7335e37effc55243a05562cfbfe845
AgUsBF <agustinbeceyro@gmail.com>
2025-11-06 21:14:26 -0300

    feat: agregar funcionalidades de los botones ver, editar y eliminar de editoriales

--- yenny/editoriales/urls.py
+++ yenny/editoriales/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import EditorialListView, EditorialCreateView, EditorialUpdateView, EditorialDeleteView
+from .views import EditorialListView, EditorialCreateView, EditorialUpdateView, EditorialDeleteView, EditorialShowView
 
 urlpatterns = [
     path('', EditorialListView.as_view(), name='editorial-list'),
     path('nuevo/', EditorialCreateView.as_view(), name='editorial-create'),
     path('editar/<int:pk>/', EditorialUpdateView.as_view(), name='editorial-update'),
     path('eliminar/<int:pk>/', EditorialDeleteView.as_view(), name='editorial-delete'),
+    path('ver/<int:pk>/', EditorialShowView.as_view(), name='editorial-show'),
 ]
--- yenny/editoriales/views.py
+++ yenny/editoriales/views.py
@@ -1,11 +1,15 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .forms import EditorialForm
 from .models import Editorial
+from django.db.models.deletion import ProtectedError
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 class EditorialListView(ListView):
     model = Editorial
     template_name = 'lista-editoriales.html'
     paginate_by = 10
+    ordering = ['nombre']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -13,11 +17,12 @@ class EditorialListView(ListView):
         objeto_pag = context['page_obj']
         registros = []
         for editorial in objeto_pag:
-            registros.append([
+            campos = [
                 editorial.id,
                 getattr(editorial, 'nombre', ''),
                 '',  # para la botonera
-            ])
+            ]
+            registros.append({'objeto': editorial, 'campos': campos})
         context.update({
             'titulo': 'Lista de Editoriales',
             'path_crear': '/editoriales/nuevo/',
@@ -25,7 +30,12 @@ class EditorialListView(ListView):
             'columnas': columnas,
             'registros': registros,
             'objeto_pag': objeto_pag,
+            'modelo': 'editorial',
         })
+
+        if self.request.GET.get('error') == 'protected':
+            context['error_message'] = "No se puede eliminar la editorial porque tiene libros asociados."
+        
         return context
 
 class EditorialCreateView(CreateView):
@@ -44,3 +54,25 @@ class EditorialDeleteView(DeleteView):
     model = Editorial
     template_name = 'editorial-confirm-delete.html'
     success_url = '/editoriales/'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.get_success_url())
+        except ProtectedError:
+            return redirect(self.success_url + '?error=protected')
+
+class EditorialShowView(EditorialUpdateView):
+    template_name = 'editorial-form.html'
+    
+    # Cargar el formulario en modo solo lectura
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        for field in form.fields.values():
+            field.widget.attrs['readonly'] = True
+            field.widget.attrs['disabled'] = True
+        return form
+
+    # No permitir POST (no guardar cambios)
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
